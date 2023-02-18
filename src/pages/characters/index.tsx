98609d094e92ded4a5388f55b58369b1ef74d3fe
Sai Teja <gstej143@gmail.com>
2023-02-18 21:52:49 +0530

    cards completed

--- src/pages/characters/index.tsx
+++ src/pages/characters/index.tsx
@@ -4,20 +4,34 @@ import { getNewsData } from "../../api";
 import * as CONSTANTS from "../../utility/constants";
 import { Box } from "@mui/system";
 import { CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import { motion } from "framer-motion";
 
 const CharactersPage = () => {
   const [characters, setCharacters] = useState<any>();
+  const [charStatus, setChardacterStatus] = useState<string>("");
 
   // init function for Characters
   const init = async () => {
     const charData = await getNewsData(CONSTANTS.CHARACTERS, { limit: 18 });
-    console.log(charData);
-    setCharacters(charData);
+
+    setCharacters(charData?.data);
+    setChardacterStatus(charData?.text);
   };
 
   useEffect(() => {
     init();
   }, []);
+
+  const hoverHandler = (id: number, hoverStatus: boolean) => {
+    const newCharData = characters?.map((char: any) => {
+      if (char.id === id) {
+        return { ...char, hoverStatus };
+      }
+      return char;
+    });
+
+    setCharacters(() => newCharData);
+  };
 
   return (
     <>
@@ -26,12 +40,12 @@ const CharactersPage = () => {
           background: "#fff",
           height: "100%",
           p: 4,
-          pt: 0,
-          mt: 2,
+
+          mt: 4,
         }}
         justifyContent="center"
       >
-        {characters?.text !== "OK" && (
+        {charStatus !== "OK" && (
           <>
             <Grid container justifyContent="center" spacing={6}>
               {Array.from(Array(18)).map((_, e) => {
@@ -44,29 +58,59 @@ const CharactersPage = () => {
             </Grid>
           </>
         )}
-        {characters?.text === "OK" && (
+        {charStatus === "OK" && (
           <>
             <Grid container justifyContent="center" spacing={6}>
-              {characters?.data?.map((char: any) => {
-                console.log(char);
+              {characters?.map((char: any) => {
                 return (
                   <Grid key={char?.id} item justifyContent="start">
-                    <Card sx={{ height: "350px", width: "250px" }}>
-                      <CardMedia
-                        component="img"
-                        height="100%"
-                        image={`${char?.thumbnail?.path}.${char?.thumbnail?.extension}`}
-                        alt="Paella dish"
-                      />
-                      <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                          {char?.name}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                          {char?.description}
-                        </Typography>
-                      </CardContent>
-                    </Card>
+                    <motion.div
+                      whileHover={{ scale: 1.1 }}
+                      onHoverStart={(e) => {
+                        console.log("hover", char?.id);
+                        hoverHandler(char?.id, true);
+                      }}
+                      onHoverEnd={() => {
+                        console.log("end", char?.id);
+                        hoverHandler(char?.id, false);
+                      }}
+                    >
+                      <Card
+                        sx={{
+                          height: "350px",
+                          width: "250px",
+                          boxShadow: 12,
+                          background: "#141414",
+                        }}
+                      >
+                        <CardMedia
+                          component="img"
+                          // height={`${char?.hoverStatus ? "75%" : "100%"}`}
+                          height="75%"
+                          image={`${char?.thumbnail?.path}.${char?.thumbnail?.extension}`}
+                          alt={`${char?.name}`}
+                          sx={{ objectFit: "fill" }}
+                        />
+                        <CardContent>
+                          <Typography
+                            gutterBottom
+                            variant="h5"
+                            component="div"
+                            sx={{
+                              pb: 6,
+                              fontWeight: "600",
+                              color: "#fff",
+                              textAlign: "center",
+                            }}
+                          >
+                            {char?.name}
+                          </Typography>
+                          {/* <Typography variant="body2" color="text.secondary">
+                            {char?.description}
+                          </Typography> */}
+                        </CardContent>
+                      </Card>
+                    </motion.div>
                   </Grid>
                 );
               })}
