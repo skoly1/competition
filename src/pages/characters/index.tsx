96780894c827a25a661b24042b219d359ca1f2a1
Sai Teja <saiteja.g1801@gmail.com>
2023-02-23 10:16:42 +0530

    Squashed commit of the following:
    commit ba325b33939464a86ff391f39b302e13a0bda018
    Author: Sai Teja <saiteja.g1801@gmail.com>
    Date:   Thu Feb 23 00:00:40 2023 +0530
    
        grid design fix

--- src/pages/characters/index.tsx
+++ src/pages/characters/index.tsx
@@ -5,6 +5,7 @@ import * as CONSTANTS from "../../utility/constants";
 import InfiniteScroll from "react-infinite-scroll-component";
 import CardComponent from "../UI Component";
 import { usePageInit, useGetPageData } from "../../hooks";
+import { Container } from "../../components";
 
 const CharactersPage = () => {
   const charReduxData = usePageInit(CONSTANTS.CHARACTERS);
@@ -17,20 +18,22 @@ const CharactersPage = () => {
 
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <InfiniteScroll
-          dataLength={charReduxData?.data?.length || 0}
-          next={fetchMore}
-          hasMore={(charReduxData?.data?.length || 0) < charReduxData?.total}
-          loader={<div>Infinite Scrolling</div>}
-          endMessage={<div>You reached End page</div>}
-        >
-          <CardComponent
-            text={charReduxData?.text}
-            characters={charReduxData?.data}
-          />
-        </InfiniteScroll>
-      </Suspense>
+      <Container>
+        <Suspense fallback={<div>Loading...</div>}>
+          <InfiniteScroll
+            dataLength={charReduxData?.data?.length || 0}
+            next={fetchMore}
+            hasMore={(charReduxData?.data?.length || 0) < charReduxData?.total}
+            loader={<div>Infinite Scrolling</div>}
+            endMessage={<div>You reached End page</div>}
+          >
+            <CardComponent
+              text={charReduxData?.text}
+              characters={charReduxData?.data}
+            />
+          </InfiniteScroll>
+        </Suspense>
+      </Container>
     </>
   );
 };
